ff2494eb4f1c19ba312f743831ca1ed41ea57ab1
PhuongNguyen29 <72432316+PhuongNguyen29@users.noreply.github.com>
2025-07-30 15:53:21 -0500

    Update R_4.2.0_compatibility_script.R
    add more solutions for dependency issues

--- R_4.2.0_compatibility_script.R
+++ R_4.2.0_compatibility_script.R
@@ -11,11 +11,22 @@ install.packages("https://cran.r-project.org/src/contrib/Archive/xfun/xfun_0.39.
 install.packages("https://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.42.tar.gz", 
                  repos = NULL, type = "source")
 
+#Caution: You might encounter error when installing htmlTable due to dependency. The common dependency is "checkmate"
+install.packages("https://cran.r-project.org/src/contrib/Archive/checkmate/checkmate_2.1.0.tar.gz", 
+                 repos = NULL, type = "source")
+
 install.packages("https://cran.r-project.org/src/contrib/Archive/htmlTable/htmlTable_2.4.0.tar.gz", 
                  repos = NULL, type = "source")
 
 # 2. Install Matrix package compatible with R 4.2.0
 install.packages("Matrix", dependencies = TRUE)
+#if above does not works, manually install older version of Matrix package
+install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.5-4.tar.gz", 
+                 repos = NULL, type = "source")
+
+#Or
+install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.4-1.tar.gz", 
+                 repos = NULL, type = "source")
 
 # 3. Install BiocManager for R 4.2.0
 if (!require("BiocManager", quietly = TRUE))
@@ -25,8 +36,18 @@ if (!require("BiocManager", quietly = TRUE))
 BiocManager::install(version = "3.16")
 BiocManager::install(c("impute", "preprocessCore", "GO.db", "AnnotationDbi"), version = "3.16")
 
+# If you get warning message:
+#Warning message:
+#package(s) not installed when version(s) same as or greater than current; use `force = TRUE` to
+#  re-install: 'AnnotationDbi' 
+
+BiocManager::install("AnnotationDbi", force = TRUE)
+
+
 # 5. Install WGCNA with specific method for Windows
 install.packages("WGCNA", type = "binary", dependencies = TRUE)
+#If using Linux, remove type = "binary"
+install.packages("WGCNA", dependencies = TRUE)
 
 # 6. Install other required packages
 required_packages <- c("tidyverse", "dendextend", "gplots", "ggplot2", 
@@ -42,6 +63,6 @@ for(pkg in required_packages) {
 BiocManager::install(c("DESeq2", "genefilter", "clusterProfiler", "org.Hs.eg.db"), 
                      version = "3.16", ask = FALSE)
 
-# 8. For ggpubr (which was problematic in the logs)
+# 8. For ggpubr
 install.packages("car", dependencies = TRUE)  # Install car first
 install.packages("ggpubr", dependencies = TRUE)
